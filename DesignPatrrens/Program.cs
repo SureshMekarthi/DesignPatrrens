a3ff3203b3a0d57658545b0a1a0e5b4ae4c4a4e4
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-08-10 15:21:57 -0400

    added Facade design pattren

--- DesignPatrrens/Program.cs
+++ DesignPatrrens/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DesignPatrrens
 {
@@ -10,6 +11,16 @@ namespace DesignPatrrens
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("--- Cleint orders pizaa---");
+
+            var serviceProvider = new ServiceCollection()
+                .AddScoped<IBread, BreadProvider>().AddSingleton<IPizza, PizzaProvider>()
+                .AddSingleton<RestaurantFacade>().BuildServiceProvider();
+
+            var facadeForCleint = serviceProvider.GetService<RestaurantFacade>();
+
+            facadeForCleint.GetVegPizza();
+            Console.WriteLine("--- Cleint orders pizaa complted---");
         }
     }
 }
---
+++ DesignPatrrens/RestaurantFacade.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatrrens
+{
+    public class RestaurantFacade
+    {
+        private IPizza _pizzaProvider;
+        private IBread _BreadProvider;
+
+        public RestaurantFacade(IPizza pizza, IBread bread)
+        {
+            _pizzaProvider = pizza;
+            _BreadProvider = bread;
+        }
+
+        public void GetNonVegPizza()
+        {
+            _pizzaProvider.GetNonvegPizza();
+        }
+
+        public void GetVegPizza()
+        {
+            _pizzaProvider.GetVegPizza();
+        }
+
+        public void GetGarlicBread()
+        {
+            _BreadProvider.GetGarlicBread();
+        }
+
+        public void GetCheesyGarlicBread()
+        {
+            _BreadProvider.GetCheesyGarlicBread();
+        }
+    }
+}
