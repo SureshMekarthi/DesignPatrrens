0240de1f8f6973edb3e64e392a9988878e400f4a
Suresh Mekarthi <sureshmekarthi@gmail.com>
2024-08-10 14:52:35 -0400

    Add project files.

---
+++ DesignPatrrens/BreadProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatrrens
+{
+    public class BreadProvider : IBread
+    {
+        public void GetCheesyGarlicBread()
+        {
+            GetCheese();
+            Console.WriteLine("Getting Cheesy Garlic Bread.");
+        }
+
+        public void GetGarlicBread()
+        {
+            Console.WriteLine("Getting Garlic Bread.");
+        }
+
+        private void GetCheese()
+        {
+            Console.WriteLine("Getting Cheese.");
+        }
+    }
+}
---
+++ DesignPatrrens/PizzaProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatrrens
+{
+    public class PizzaProvider : IPizza
+    {
+        public void GetNonvegPizza()
+        {
+            GetNonVegToppings();
+            Console.WriteLine("Getting Non Veg Pizza.");
+        }
+
+        public void GetVegPizza()
+        {
+            Console.WriteLine("Getting  Veg Pizza.");
+        }
+
+        private void GetNonVegToppings()
+        {
+            Console.WriteLine("Getting Non Veg Pizza Toppings.");
+        }
+    }
+}
